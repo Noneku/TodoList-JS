b1dd41b386c7b0d995b32cbe7d7764fa8856db0b
Noneku <dev.nassim.pro@gmail.com>
2022-05-18 19:54:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let Vrai = false;
+
+switch(Vrai){
+    case true:
+        console.log("Cette information est vrai");
+    
+    break;
+
+    default: 
+    console.log("Cette information est fausse");
+}
+
